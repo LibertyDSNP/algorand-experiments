17d3d57dc553f08d91f9140c733f5e27335b2ebd
shannonwells <shannonwells@users.noreply.github.com>
2021-10-22 17:45:51 -0700

    registration contract with opt-in and receiving a DSNP id on opt-in

--- python/helpers.py
+++ python/helpers.py
@@ -3,8 +3,15 @@ from algosdk import account, mnemonic
 from algosdk.future import transaction
 
 
+# logic.get_application_address(appID)
+# avm sdk pooled groups
+# pytealdbg ???  make a dry run request / txn?
+
+
 def get_funded_account(index=0):
-    # this mnemonic is for a consistently generated sandbox account with lots of algos.
+    # these mnemonics are for consistently generated sandbox localhost accounts
+    # with lots of algos.
+    # Address: W5SZASJBHX7SXDZ7D65G75AWLTCFOPJMNWMQRYPUOWK6R4NGU3CKSL65MA
     sender_mnemonic = "metal morning betray sand have banner drum kiss fossil orbit pipe salt once unique fire lady bubble ethics visit junior patrol wire fortune abstract bright"
 
     if (index >0):
@@ -19,18 +26,26 @@ def generate_algorand_keypair():
     private_key, address = account.generate_account()
     return private_key, address
 
-# helper function to compile program source
+# helper function to compile program source.
+# The hash is the address.
 def compile_program(client, source_code):
     compile_response = client.compile(source_code)
     return compile_response['result'], compile_response['hash']
 
 # returns the contract address + the contract code as bytes for using in a
-# ApplicationCreate transaction.
+# ApplicationCreate transaction.  The hash is the addres.
 def compile_program_to_bytes(client, source_code) :
     compile_response = client.compile(source_code)
     return compile_response['hash'], base64.b64decode(compile_response['result'])
 
 # create new application
+# parameters:
+#  algod_client - the algod client
+#  sender_priv - private key of the transaction sender, which will be the application creator
+#  approval_program - the approval program in base64 decoded bytes (see compile_program_to_bytes)
+#  clear_program - the clear program in base64 decoded bytes
+#  global_schema - the global schema for the contract
+#  local_schema - the local schema for the contract
 def create_app(algod_client, sender_priv, approval_program, clear_program, global_schema, local_schema):
     # define sender as creator
     sender = account.address_from_private_key(sender_priv)
@@ -62,8 +77,14 @@ def create_app(algod_client, sender_priv, approval_program, clear_program, globa
 
     return app_id
 
-# Accounts can only opt into up to 50 smart contracts. Accounts may only create 10 smart contracts.
-# This creates a Logic Signature transaction using the provided logic 'registry_smart_sig_program'
+# Accounts can opt into up to 50 smart contracts. Accounts may only create 10 smart contracts.
+# This creates a Logic Signature payment transaction using the provided logic
+# params:
+#  smart_sig_program - the LogicSig of the smart signature program
+#  smart_sig_addr - the address (hash) of the smart signature program once it's compiled.
+#  amt - the amount to transfer
+#  reciever_addr - the recipient of 'amt'
+#  algod_client - the algod client
 def lsig_payment_txn(smart_sig_program, smart_sig_addr, amt, receiver_addr, algod_client):
     params = algod_client.suggested_params()
     upayment_txn = transaction.PaymentTxn(smart_sig_addr, params, receiver_addr, amt)
@@ -76,10 +97,13 @@ def lsig_payment_txn(smart_sig_program, smart_sig_addr, amt, receiver_addr, algo
     pmtx = wait_for_confirmation(algod_client, tx_id, 10)
     return pmtx
 
-def payment_transaction(creator_mnemonic, amt, rcv, algod_client)->dict:
+# Perform an ordinary payment transaction
+# params:
+#   sender_mnemonic
+def payment_transaction(sender_mnemonic, amt, rcv, algod_client)->dict:
     params = algod_client.suggested_params()
-    senderAddr = mnemonic.to_public_key(creator_mnemonic)
-    senderPrivKey = mnemonic.to_private_key(creator_mnemonic)
+    senderAddr = mnemonic.to_public_key(sender_mnemonic)
+    senderPrivKey = mnemonic.to_private_key(sender_mnemonic)
     utxn = transaction.PaymentTxn(senderAddr, params, rcv, amt)
     stxn = utxn.sign(senderPrivKey)
     txid = algod_client.send_transaction(stxn)
--- python/registry_contract.py
+++ python/registry_contract.py
@@ -17,16 +17,43 @@ algod_token = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
 # this is the registration contract
 # TODO: it should generate a new DSNPId and update its global state to the new id.
 # TODO: it should also be able to insert a new handle into a caller's local state
+# global state:
+#    d:  latest DSNPId, int
+#    h:  last handler called, bytes:
+#           c = create
+#           n = no-op
+#           o = optin
+#           l = closeout
+#           u = update
+#           d = delete
 def approval_program_teal(owner_addr):
-    # program = Cond(
-    #     [Txn.application_id() == Int(0), handle_creation],
-    #     [Txn.on_completion() == OnComplete.OptIn, handle_optin],
+    handle_creation = Seq([
+        App.globalPut(Bytes("d"), Int(999)),
+        App.globalPut(Bytes("h"),Bytes("c")),
+        Return(Int(1))
+    ])
+
+    scratchCount = ScratchVar(TealType.uint64)
+
+    #
+    handle_optin = Seq([
+        scratchCount.store(App.globalGet(Bytes("d"))),
+        App.globalPut(Bytes("d"), scratchCount.load() + Int(1)),
+        App.globalPut(Bytes("h"), Bytes("o")),
+        App.localPut(Txn.sender(), Bytes("d"), App.globalGet(Bytes("d"))),
+        Return(Int(1))
+    ])
+
+    handle_noop = Return(Int(1))
+
+    program = Cond(
+        [Txn.application_id() == Int(0), handle_creation],
+        [Txn.on_completion() == OnComplete.OptIn, handle_optin],
     #     [Txn.on_completion() == OnComplete.CloseOut, handle_closeout],
     #     [Txn.on_completion() == OnComplete.UpdateApplication, handle_updateapp],
     #     [Txn.on_completion() == OnComplete.DeleteApplication, handle_deleteapp],
-    #     [Txn.on_completion() == OnComplete.NoOp, handle_noop]
-    # )
-    program = Return(Int(1))
+        [Txn.on_completion() == OnComplete.NoOp, handle_noop]
+    )
     return compileTeal(program, Mode.Application, version=5)
 
 def clear_program_teal():
@@ -72,19 +99,23 @@ def optin_transaction(sender_addr, lsig, app_id, algod_client) -> dict:
 
 # min: 100,000 microalgos
 def main():
-    sender_addr, sender_privKey, sender_mnemonic = get_funded_account()
-    # recipient_privKey, recipentAddr = generate_algorand_keypair()
+
+    # reg_owner will be the owner of the registration contract.
+    reg_owner_addr, reg_owner_privKey, reg_owner_mnemonic = get_funded_account(0)
+
+
+    user_privKey, user_addr, user_mnemonic = get_funded_account(1)
 
     algod_client = algod.AlgodClient(algod_token, algod_address)
-    application_owner_addr = sender_addr
-    registry_global_schema = transaction.StateSchema(1,0)  # DSNPid current
-    registry_local_schema = transaction.StateSchema(1, 1)  # DSNPid, handles
+    application_owner_addr = reg_owner_addr
+    registry_global_schema = transaction.StateSchema(1,1)  # DSNPid current, last called handler
+    registry_local_schema = transaction.StateSchema(1, 1)  # DSNPid, handles registered
     _, approval_program = compile_program_to_bytes(algod_client, approval_program_teal(application_owner_addr))
     _, clear_program = compile_program_to_bytes(algod_client, clear_program_teal())
 
     mainRegistrationContract_appID = create_app(
         algod_client,
-        sender_privKey,
+        reg_owner_privKey,
         approval_program,
         clear_program,
         registry_global_schema,
@@ -98,7 +129,7 @@ def main():
     print("smart signature program:", program)
     print("smart signature hash: ", stateless_reg_contract_addr)
     print("--------------------------------------------")
-    print("Activating Registration Smart Signature......")
+    print("Activating Registration Smart Signature")
 
     # Activate contract by sending
     # 100k microalgo minimum balance +
@@ -106,17 +137,19 @@ def main():
     # + 168,999 for . . . ? the program size?
     # 1 microalgo for transaction fee from creator
     # Error:  balance 109001 below min 278500
+
     amt = 300000
-    payment_transaction(sender_mnemonic, amt, stateless_reg_contract_addr, algod_client)
+    #  the DSNP user will activate the smart signature and use it to opt in to the
+    # main registration contract
+    payment_transaction(user_mnemonic, amt, stateless_reg_contract_addr, algod_client)
     print("--------------------------------------------")
     print("Call Smart Signature to opt in")
     encoded_prog = program.encode()
     decodedBytes = base64.decodebytes(encoded_prog)
     lsig = transaction.LogicSig(decodedBytes)
 
-
     # send an opt-in transaction to the contract, signed by smart signature contract owner
-
-    optin_transaction(stateless_reg_contract_addr, lsig, mainRegistrationContract_appID, algod_client)
-
+    txn = optin_transaction(stateless_reg_contract_addr, lsig, mainRegistrationContract_appID, algod_client)
+    print("------------ OPT IN TXN RESULT: ------------")
+    print(txn)
 main()
